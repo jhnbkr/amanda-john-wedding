4df7c316a267e4163eab758fd6dc36f6a74aecbe
John Baker <john.baker@aspireiq.com>
2025-04-26 19:18:16 -0400

    refactor: update menu item subtext

--- src/app/components/RSVP/constants.ts
+++ src/app/components/RSVP/constants.ts
@@ -3,22 +3,19 @@ import { MenuItem } from "./types";
 export const APPETIZERS: MenuItem[] = [
   {
     title: "Artisanal Mixed Greens",
-    subtext: "Fresh seasonal greens with house vinaigrette",
   },
   {
     title: "Caprese Salad",
-    subtext: "Fresh mozzarella, heirloom tomatoes, basil, balsamic reduction",
   },
   {
     title: "Caesar Salad",
-    subtext: "Crisp romaine, smoked pork belly, garlic croutons, parmesan",
   },
 ];
 
 export const ENTREES: MenuItem[] = [
   {
     title: "AAA Beef Striploin",
-    subtext: "Roasted potatoes, seasonal vegetables, red wine beef jus",
+    subtext: "Roasted potatoes, seasonal vegetables, beef jus",
   },
   {
     title: "Chicken Supreme",
@@ -26,22 +23,19 @@ export const ENTREES: MenuItem[] = [
   },
   {
     title: "Butternut Squash Ravioli",
-    subtext: "Chardonnay sage butter sauce, toasted pine nuts, crispy sage (V)",
+    subtext: "Chardonnay sage butter sauce (V)",
   },
 ];
 
 export const DESSERTS: MenuItem[] = [
   {
     title: "Strawberry Mason Shortcake",
-    subtext: "Fresh strawberries, vanilla cream, buttery shortcake crumble",
   },
   {
     title: "Chocolate Truffle Mousse",
-    subtext: "Dark chocolate, fresh berries, chantilly cream",
   },
   {
     title: "Baked Apple Betty",
-    subtext: "Warm spiced apples, brown sugar streusel, vanilla bean ice cream",
   },
 ];
 
